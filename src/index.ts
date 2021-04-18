c4e869fe20fba3928ef5908fa708e3c4425cf4ea
Lucas Borinato <lucasborinato@gmail.com>
2021-04-18 00:53:08 -0300

    Adicionado a configuracao de CORS

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,26 @@ import { produtoRouter } from './routes/produto';
 
 const app = express();
 app.use(json());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(produtoRouter);
 
 mongoose.connect('mongodb://localhost:27017/adega', {
