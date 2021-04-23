add46806b0abc6dff84e246812c99b4d940d145e
Marcelo R. Vismari <marcelo.vismari@hotmail.com>
2021-04-23 15:41:00 -0300

    + ValidateJWTMiddleware - responsável por validar o JWT nas requisições

---
+++ src/infra/validate-jwt.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import jsonWebToken, { Secret } from 'jsonwebtoken';
+
+/**
+ * Middleware resopnsável por validar o JWT passado no header
+ * Authorization.
+ * @param req Objeto request
+ * @param res Objeto response
+ * @param next NextFunction para passar para o próximo middleware ou rota
+ */
+function ValidateJWTMiddleware(req: Request, res: Response, next: NextFunction) {
+    const jwt = req.headers['authorization'] as string;
+    const chavePrivada = (process.env.SECRET as Secret);
+
+    // Efetuando a validação do JWT:
+    jsonWebToken.verify(jwt, chavePrivada, (err, jwtPayload) => {
+        if (err) {
+            res.status(403).end('');
+            return;
+        }
+
+        (req as any).session = jwtPayload;
+        next();
+    });
+}
+
+export default ValidateJWTMiddleware;
