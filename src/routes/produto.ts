48c195a7810165e3c96c68e4d67945f66389543c
Lucas Borinato <lucasborinato@gmail.com>
2021-05-10 20:00:19 -0300

    Atualizado o CRUD produto

--- src/routes/produto.ts
+++ src/routes/produto.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response } from 'express';
-import { ObjectId } from '..';
-
 import { buildResponse } from '../infra/buildResponse';
 import ValidateJWTMiddleware from '../infra/validate-jwt.middleware';
 import { FatorConversao, IFatorConversao } from '../models/fator-conversao';
 import { IProduto, Produto } from '../models/produto';
 
+
 const router = express.Router()
 
 router.post('/api/produtos', ValidateJWTMiddleware, async (req: Request, res: Response) => {
     try {
-       
+
         if (!req.body.descricao) {
             throw 'Campo "descricao" precisa ser informado';
         }
@@ -28,15 +27,15 @@ router.post('/api/produtos', ValidateJWTMiddleware, async (req: Request, res: Re
         }
 
         let body = req.body;
-        
+
         FatorConversao.find({
             '_id': { $in: body.fatoresConversao }
         }, async (error, success: IFatorConversao[]) => {
 
-            if(error) {
+            if (error) {
                 throw error;
             }
-            
+
             body.fatoresConversao = success;
 
             const produto: IProduto = new Produto({
@@ -45,19 +44,19 @@ router.post('/api/produtos', ValidateJWTMiddleware, async (req: Request, res: Re
                 valor: body.valor,
                 fatoresConversao: body.fatoresConversao
             });
-    
+
             await produto.save();
             buildResponse(res, { msg: 'Produto incluído com sucesso' });
         });
-        
-       
+
+
     } catch (err) {
         buildResponse(res, { err });
     }
 });
 
 router.get('/api/produtos', ValidateJWTMiddleware, async (req: Request, res: Response) => {
-    try {       
+    try {
         const produtos = await Produto.find();
         buildResponse(res, { dados: produtos });
     } catch (err) {
@@ -66,32 +65,36 @@ router.get('/api/produtos', ValidateJWTMiddleware, async (req: Request, res: Res
 });
 
 router.get('/api/produtos/:produtoId', ValidateJWTMiddleware, async (req: Request, res: Response) => {
-    try {       
-        Produto.findOne({ '_id': req.params.produtoId })
-            .then(dados => {
-                buildResponse(res, { dados });
-            })
-            .catch(_ => {
-                buildResponse(res, null);
-            });
+    try {
+        let produto;
+        try {
+            produto = await Produto.findOne({ '_id': req.params.produtoId });
+            if (!produto) {
+                throw 'Produto não encontrado';
+            }
+        } catch {
+            throw 'Produto não encontrado';
+        }
+
+        buildResponse(res, { dados: produto });
+
     } catch (err) {
         buildResponse(res, { err });
     }
 });
 
 router.delete('/api/produtos/:produtoId', ValidateJWTMiddleware, async (req: Request, res: Response) => {
-    try {       
-        Produto.deleteOne({ '_id': req.params.produtoId })
-            .then(dados => {
-                if (dados?.deletedCount?.toString() == '0') {
-                    buildResponse(res, { msg: 'Produto não encontrado' });
-                } else {
-                    buildResponse(res, { msg: 'Produto excluído com sucesso' });
-                }
-            })
-            .catch(error => {
-                throw error;
-            });
+    try {
+        try {
+            const produto = Produto.deleteOne({ '_id': req.params.produtoId });
+            if (produto && (produto as any).deletedCount === 0) {
+                throw 'Produto não encontrado';
+            }
+        } catch {
+            throw 'Produto não encontrado';
+        }
+
+        buildResponse(res, { msg: 'Produto excluído com sucesso' });
     } catch (err) {
         buildResponse(res, { err });
     }
@@ -99,7 +102,7 @@ router.delete('/api/produtos/:produtoId', ValidateJWTMiddleware, async (req: Req
 
 router.put('/api/produtos/:produtoId', ValidateJWTMiddleware, async (req: Request, res: Response) => {
     try {
-        
+
         if (!req.body.descricao) {
             throw 'Campo "descricao" precisa ser informado';
         }
@@ -118,27 +121,30 @@ router.put('/api/produtos/:produtoId', ValidateJWTMiddleware, async (req: Reques
 
         let body = req.body;
 
-        FatorConversao.find({
+        let fatoresConversao = await FatorConversao.find({
             '_id': { $in: body.fatoresConversao }
-        }, async (error, success: IFatorConversao[]) => {
+        });
 
-            if(error) {
-                throw error;
+        if (!fatoresConversao) {
+            throw 'Fatores de conversão não encontrados';
+        }
+
+        body.fatoresConversao = fatoresConversao;
+
+        try {
+            let produto = await Produto.findOne({ '_id': req.params.produtoId });
+            if (!produto) {
+                throw 'Produto não encontrado';
             }
-            
-            body.fatoresConversao = success;
+        } catch {
+            throw 'Produto não encontrado';
+        }
 
-            Produto.updateOne({ '_id': req.params.produtoId }, body)
-            .then(_ => {
-                buildResponse(res, { msg: 'Produto atualizado com sucesso' });
-            })
-            .catch(_ => {
-                buildResponse(res, null);
-            });
-        });        
+        await Produto.updateOne({ '_id': req.params.produtoId }, body);
+        buildResponse(res, { msg: 'Produto atualizado com sucesso' });
     } catch (err) {
         buildResponse(res, { err });
     }
 });
 
-export { router as produtoRouter }
+export { router as produtoRouter };
